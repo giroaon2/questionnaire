be9610679ed2d41ae36255bb5ffe047f440084a9
Aon <giroaon2@gmail.com>
2021-05-08 11:25:06 +0700

    get data from form

--- questionnaire/questionnaire/server.js
+++ questionnaire/questionnaire/server.js
@@ -1,18 +1,31 @@
 'use strict';
 var http = require('http');
 var port = process.env.PORT || 1337;
+var formidable = require('formidable');
 var fs = require('fs');
  
-function onRequest(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.writeHead('File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
+function onRequest(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    if (req.url == '/submit') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            console.log(fields.FirstName);
+            loadPage('./submit.html', res);
+        });
+    } else {
+        loadPage('./index.html', res);
+    }
 }
 http.createServer(onRequest).listen(port);
+
+function loadPage(page, res) {
+    fs.readFile(page, null, function (error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.writeHead('File not found!');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
+}
